c71bdda2a46f65ebaaae4fae82d1cd35e913d8d0
Gerard Doncel <gerard@tresipunt.com>
2023-02-09 18:44:23 +0100

    Preparar estructura de datos

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-
-const getPokemons = () => {
-    
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-    .then(response => {
-        return response.json();
-    })
+const getPokemons = () => fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    .then(response => {return response.json();})
     .then(data => {
-        console.log(data);
+        const arr = Object.values(data.results);
 
-        createPokemonTable();
+        console.log(arr);
+        
+        arr.foreach(row =>
+            console.log(row.url)
+        );
     });
-}
 
 const pokemonDiv = document.querySelector("div.pokemon-table");
+
+let tableHeaders = ["Sprite", "Name", "Primary Type", "Secondary Type"]
 
 const createPokemonTable = () => {
 
@@ -21,6 +21,25 @@ const createPokemonTable = () => {
 
     let pokemonTableHead = document.createElement('thead');
     pokemonTableHead.className = 'pokemonTableHead';
+
+    let pokemonTableHeadRow = document.createElement('tr');
+    pokemonTableHeadRow.className = 'pokemonTableHeadRow'
+
+    tableHeaders.forEach(header => {
+        let pokemonHeader = document.createElement('th')
+        pokemonHeader.innerText = header
+        pokemonTableHeadRow.append(pokemonHeader)
+    })
+
+    pokemonTableHead.append(pokemonTableHeadRow);
+    pokemonTable.append(pokemonTableHead);
+    
+    let pokemonTableBody = document.createElement('tbody')
+    pokemonTableBody.className = "pokemonTableBody"
+    pokemonTable.append(pokemonTableBody)
+    
+    pokemonDiv.append(pokemonTable)
+
 }
 
 getPokemons();
