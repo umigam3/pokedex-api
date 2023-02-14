b7f5e113897aac9eb9e8dd0bfff5f2b0536e1eba
Umigame <gerarddoncel41@gmail.com>
2023-02-14 20:17:53 +0100

    Responsive 5 Gen

--- js/script.js
+++ js/script.js
@@ -137,5 +137,5 @@ function search() {
 }
   
 window.onload = function() {
-    getData("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0");
+    getData("https://pokeapi.co/api/v2/pokemon?limit=649&offset=0");
 }
